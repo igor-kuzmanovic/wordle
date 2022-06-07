2f0f22c728658326b2fd8cf35b67b0623393e2b3
igor-kuzmanovic <kuzman.dev@gmail.com>
2022-06-07 21:17:58 +0200

    update dependencies

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders heading", () => {
-  render(<App />);
-  const headingElement = screen.getByRole('heading');
-  expect(headingElement).toBeInTheDocument();
-});
