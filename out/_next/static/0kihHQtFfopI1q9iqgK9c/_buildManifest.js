cf573d01a3eace666540dce7694f4db7d4713b9c
igor-kuzmanovic <kuzman.dev@gmail.com>
2022-10-01 14:28:35 +0200

    Deploy

---
+++ out/_next/static/0kihHQtFfopI1q9iqgK9c/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a13bd18dec10a289.css","static/chunks/pages/index-6490395857f4a01b.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
